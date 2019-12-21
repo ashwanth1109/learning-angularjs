4d14e146301e52172440a0462db7c7d2f9e4c4e4
Ashwanth <ashwanth1109@gmail.com>
2019-12-21 23:48:57 +0530

    Running a function from Controller in View

--- section1/app.js
+++ section1/app.js
@@ -3,6 +3,9 @@ const app = angular.module("app", []);
 app.controller("MainController", [
   "$scope",
   function($scope) {
-    $scope.greeting = "Hola!";
+    $scope.greeting = "Hola,";
+    $scope.name = function() {
+      return "Ashwanth" + " " + "A R";
+    };
   }
 ]);
