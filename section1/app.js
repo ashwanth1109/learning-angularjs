57822020d65473d4c5d43c6b52c04575de65903c
Ashwanth A R <ashwanth1109@gmail.com>
2019-12-25 18:03:11 +0530

    Learn: ng-repeat

--- section1/app.js
+++ section1/app.js
@@ -21,6 +21,20 @@ app.controller("MainController", [
   }
 ]);
 
+app.controller("ListController", [
+  "$scope",
+  function($scope) {
+    $scope.myArr = [1, 2, 3, 4, 5, 5];
+    $scope.users = [
+      { name: "Mike", age: 23 },
+      { name: "Andy", age: 34 },
+      { name: "Reid", age: 45 }
+    ];
+
+    $scope.heroes = { name: "Kunkka", attack: "Melee", type: "Strength" };
+  }
+]);
+
 angular.module("controllers", []).controller("MainController1", [
   "$scope",
   function($scope) {
